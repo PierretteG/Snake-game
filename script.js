1f2afd75b6c8268fe9bf8fc12e7257b2b4ffc240
Petra <petragerzova@gmail.com>
2018-07-17 22:19:49 +0200

    adding canvas element

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-// coordinates for the position of the snake
-let snakeX = 100,
-    snakeY = 100;
+let canvas = document.querySelector('canvas');
 
-let snake = document.querySelector('#snake');
-
-// coordinates for the position of the food
-let foodX = 0,
-    foodY = 0,
-    foodWidth = 20,
-    foodHeight = 20;
-
-let food = document.querySelector('#food');
-
-placeTheSnake();
-placeTheFood();
-
-function placeTheSnake() {
-    snake.style.left = snakeX + 'px';
-    snake.style.top = snakeY + 'px';
-}
-
-function placeTheFood() {
-    foodX = Math.floor(Math.random() * (window.innerWidth - foodWidth));
-    foodY = Math.floor(Math.random() * (window.innerHeight - foodHeight));
-
-    food.style.left = foodX + 'px';
-    food.style.top = foodY + 'px';
-}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
