6bf2bbe1c5dbaeb16764d9445d06e1822f16a2c6
Petra <petragerzova@gmail.com>
2018-07-17 23:01:10 +0200

    adding randomly positioned element.

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ let canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let c = canvas.getContext('2d');
+
+let x = Math.floor(Math.random() * (canvas.width - 20));
+let y = Math.floor(Math.random() * (canvas.height - 20));
+
+c.fillStyle = 'rgb(60, 60, 60)';
+c.fillRect(50, 50, 20, 20);
+
+c.fillStyle = 'rgb(244, 40, 40)';
+c.fillRect(x, y, 20, 20);
