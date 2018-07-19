65555aa77921d5108ae5ecf5be094488e0142791
Petra <petragerzova@gmail.com>
2018-07-19 09:52:11 +0200

    adding keydown event

--- script.js
+++ script.js
@@ -5,11 +5,62 @@ canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
 
-let x = Math.floor(Math.random() * (canvas.width - 20));
-let y = Math.floor(Math.random() * (canvas.height - 20));
+let snakeX = 50,
+    snakeY = 50,
+    snakeWidth = 20,
+    snakeHeight = 20;
 
-c.fillStyle = 'rgb(60, 60, 60)';
-c.fillRect(50, 50, 20, 20);
+//velocity of the snake move
+let dx = 4;
 
-c.fillStyle = 'rgb(244, 40, 40)';
-c.fillRect(x, y, 20, 20);
+let foodX = Math.floor(Math.random() * (canvas.width - 20)),
+    foodY = Math.floor(Math.random() * (canvas.height - 20)),
+    foodWidth = 20,
+    foodHeight = 20;
+
+
+
+function createSnake() {
+    requestAnimationFrame(createSnake);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    c.fillStyle = 'rgb(60, 60, 60)';
+    c.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
+
+
+    c.fillStyle = 'rgb(244, 40, 40)';
+    c.fillRect(foodX, foodY, foodWidth, foodHeight);
+
+}
+
+function moveSnake(event) {
+    if (event.keyCode === 39) {
+        snakeX += dx;
+
+        if (snakeX > canvas.width - snakeWidth) {
+            snakeX = canvas.width - snakeWidth;
+        }
+    } else if (event.keyCode === 38) {
+        snakeY -= dx;
+
+        if (snakeY = 0) {
+            snakeY = 0;
+        }
+    } else if (event.keyCode === 37) {
+        snakeX -= dx;
+
+        if (snakeX = 0) {
+            snakeX = 0;
+        }
+    } else if (event.keyCode === 40) {
+        snakeY += dx;
+
+        if (snakeY > canvas.height - snakeHeight) {
+            snakeY = canvas.height - snakeWidth;
+        }
+    }
+}
+
+
+
+window.addEventListener("keydown", moveSnake);
